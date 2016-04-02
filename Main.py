24306bc7978628a4bfe414f28c32ff0b53b50290
Ian Holst <ianholst@gmail.com>
2016-04-02 01:14:44 -0400

    Updated event handling for main class

--- Main.py
+++ Main.py
@@ -1,11 +1,15 @@
 from tkinter import *
 import sys
+#from Game import *
+#from LED import *
+#from Buttons import *
 
 class Main:
 
-	def __init__(self, game=None, leds=None):
+	def __init__(self, game=None, leds=None, buttons=None):
 		self.game = game
 		self.leds = leds
+		self.buttons = buttons
 
 		# Tk interface
 		self.root = Tk()
@@ -17,26 +21,36 @@ class Main:
 			self.root.minsize(width=self.windowWidth, height=self.windowHeight)
 			self.root.maxsize(width=self.windowWidth, height=self.windowHeight)
 			self.root.attributes('-fullscreen', True)
+		self.root.bind("<Escape>", lambda event: self.root.attributes("-fullscreen", False))
 		self.canvas = Canvas(self.root, width=self.windowWidth, height=self.windowHeight, background="black")
 		self.canvas.pack()
 		self.timerDelay = 100 #ms
 
 	def run(self):
-		self.timerFired()
+		# Starts update and Tkinter loops
+		self.update()
 		self.root.mainloop()
 
-	def timerFired(self):
+	def update(self):
 		# Check for button input
-		# Pass input to the game
-		# Update the game
-		self.redrawAll()
-		self.canvas.after(self.timerDelay, self.timerFired)
+		# Store current input state
+			# Update LEDs
+		# If commit button is pressed
+			# check for valid input
+				# Pass input to the game
+				# Update the game
+		# Update LEDs
+		self.redraw()
+		self.canvas.after(self.timerDelay, self.update)
 
-	def redrawAll(self):
+	def redraw(self):
 		self.canvas.delete(ALL)
 		# Drawing code here
 		self.canvas.update()
 
+game = Game()
+leds = LED()
+buttons = Buttons()
 
-main = Main()
+main = Main(game, leds, buttons)
 main.run()
