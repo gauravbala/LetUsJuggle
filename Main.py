c79b5f2a6344a5d925d0a8281aabbf00676cb6d0
Ian Holst <ianholst@gmail.com>
2016-04-02 09:51:27 -0400

    More integration

--- Main.py
+++ Main.py
@@ -1,37 +1,37 @@
 from tkinter import *
 import sys
-#from Game import *
-#from LED import *
-#from Buttons import *
+from mastermind import *
+from Mastermind_Hardware import *
 
 class Main:
 
 	def __init__(self, game=None, leds=None, buttons=None):
-		self.game = game
-		self.leds = leds
-		self.buttons = buttons
+		self.game = Mastermind()
+		self.leds = LED()
+		self.buttons = Buttons()
 
 		# Tk interface
 		self.root = Tk()
 		self.root.title("Mastermind")
-		# Run this code if on the raspberry pi
 		self.windowWidth = 800
 		self.windowHeight = 480
+		# Run this code if on the raspberry pi
 		if sys.platform.startswith('linux'):
 			self.root.minsize(width=self.windowWidth, height=self.windowHeight)
 			self.root.maxsize(width=self.windowWidth, height=self.windowHeight)
 			self.root.attributes('-fullscreen', True)
-		self.root.bind("<Escape>", lambda event: self.root.attributes("-fullscreen", False))
+			# Exit fullscreen with Esc
+			self.root.bind("<Escape>", lambda event: self.root.attributes("-fullscreen", False))
 		self.canvas = Canvas(self.root, width=self.windowWidth, height=self.windowHeight, background="black")
 		self.canvas.pack()
-		self.timerDelay = 100 #ms
+		self.timerDelay = 10 #ms
 
 	def run(self):
 		# Starts update and Tkinter loops
-		self.update()
+		self.timerFired()
 		self.root.mainloop()
 
-	def update(self):
+	def timerFired(self):
 		# Check for button input
 		# Store current input state
 			# Update LEDs
@@ -39,18 +39,15 @@ class Main:
 			# check for valid input
 				# Pass input to the game
 				# Update the game
+				# Check for game over
 		# Update LEDs
 		self.redraw()
-		self.canvas.after(self.timerDelay, self.update)
+		self.canvas.after(self.timerDelay, self.timerFired)
 
-	def redraw(self):
+	def redrawAll(self):
 		self.canvas.delete(ALL)
 		# Drawing code here
 		self.canvas.update()
 
-game = Game()
-leds = LED()
-buttons = Buttons()
-
-main = Main(game, leds, buttons)
+main = Main()
 main.run()
--- mastermind.py
+++ mastermind.py
@@ -1,37 +1,42 @@
 # make mastermind
 import random
-class Mastermind(object):
-    def __init__(self):
-        self.colors=["red","green","blue","pink","cyan","yellow"] # list of LED colors
-        self.code=[random.choice(self.colors) for i  in range(len(self.colors))]
-        self.guess=[None]*4
-        self.turns=1
-        self.exact=0
-        self.offPlace=0
-        self.win=False  # use this  to check for win whe gameOver returns True 
 
-    def  makeMove(self):
+class Mastermind(object):
+
+    def __init__(self):
+        self.colors = ["red","green","blue","pink","cyan","yellow"] # list of LED colors
+        self.code = [random.choice(self.colors) for i  in range(len(self.colors))]
+        self.guess = [None]*4
+        self.turns = 1
+        self.exact = 0
+        self.offPlace = 0
+        self.win = False  # use this  to check for win whe gameOver returns True 
+
+    def makeMove(self):
         # update guess through LEDs
-        self.turns+=1
+        self.turns += 1
 
     def checkMove(self):
         self.exactMatches()
         self.offPlaceMatches()
+
     def exactMatches(self):
-        self.exact=0
+        self.exact = 0
         for i in range(4):
-            if self.code[i]==self.guess[i]:
-                self.exact+=1
+            if self.code[i] == self.guess[i]:
+                self.exact += 1
+
     def offPlaceMatches(self):
-        self.offPlace=0
+        self.offPlace = 0
         for i in range(4):
             for j in range(4):
-                if i!=j and self.code[i]==self.guess[i]:
-                    self.offPlace+=1
+                if i != j and self.code[i] == self.guess[i]:
+                    self.offPlace += 1
+
     def gameOver(self):
-        if self.code==self.guess:
-            self.win=True
+        if self.code == self.guess:
+            self.win = True
             return True
-        if self.turns==8:
+        if self.turns == 8:
             return True
         return False
